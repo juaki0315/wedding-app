6e17a10c1b5ad3c3a7cb18938d4bd2c8ceb87b2e
juaki0315 <juaki0315@gmail.com>
2023-09-19 15:15:08 +0200

    registro

--- app.js
+++ app.js
@@ -1,41 +1,31 @@
-// Variables globales
-const registrationForm = document.getElementById('registrationForm');
-const questionnaire = document.getElementById('questionnaire');
-const nameInput = document.getElementById('nameInput');
-const registerBtn = document.getElementById('registerBtn');
-const submitBtn = document.getElementById('submitBtn');
+const nombre = document.getElementById("name")
+const email = document.getElementById("email")
+const pass = document.getElementById("password")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
 
-// Comprobar si el usuario ya está registrado
-const user = localStorage.getItem('user');
-if (user) {
-  showQuestionnaire();
-} else {
-  showRegistrationForm();
-}
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    parrafo.innerHTML = ""
+    if(nombre.value.length <6){
+        warnings += `El nombre no es valido <br>`
+        entrar = true
+    }
+    if(!regexEmail.test(email.value)){
+        warnings += `El email no es valido <br>`
+        entrar = true
+    }
+    if(pass.value.length < 8){
+        warnings += `La contraseña no es valida <br>`
+        entrar = true
+    }
 
-// Evento para registrar al usuario
-registerBtn.addEventListener('click', () => {
-  const name = nameInput.value.trim();
-  if (name !== '') {
-    localStorage.setItem('user', name);
-    showQuestionnaire();
-  }
-});
-
-// Evento para enviar las respuestas del cuestionario
-submitBtn.addEventListener('click', () => {
-  // Aquí puedes obtener las respuestas del cuestionario y guardarlas en el backend (por ejemplo, en Firebase)
-  alert('¡Gracias por completar el cuestionario!');
-});
-
-// Función para mostrar el formulario de registro
-function showRegistrationForm() {
-  registrationForm.style.display = 'block';
-  questionnaire.style.display = 'none';
-}
-
-// Función para mostrar el cuestionario
-function showQuestionnaire() {
-  registrationForm.style.display = 'none';
-  questionnaire.style.display = 'block';
-}
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Enviado"
+    }
+})
